6341693f07de7bc959a55a0989f82062f3e7be22
venkatesh <venkatesh.sv645@gmail.com>
2021-07-04 15:23:10 +0530

    initial commit

---
+++ src/main/java/com/one/experiment1/Experiment1Application.java
@@ -0,0 +1,25 @@
+package com.one.experiment1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class Experiment1Application {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Experiment1Application.class, args);
+	}
+
+	public void run(String... args) throws Exception{
+		String sql = "INSERT INTO students (name) VALUES ('sam');";
+		int rows = jdbcTemplate.update(sql);
+
+
+	}
+
+}
---
+++ src/test/java/com/one/experiment1/Experiment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.experiment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
