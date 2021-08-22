2522f5fe22391b2780067a767016a7bc655e5b12
venkatesh <venkatesh.sv645@gmail.com>
2021-08-22 23:20:50 +0530

    correlation ID added

--- src/main/java/com/one/experiment1/MdcInterceptor.java
+++ src/main/java/com/one/experiment1/MdcInterceptor.java
@@ -16,13 +16,14 @@ public class MdcInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     String CID = getCorrelationId();
-    log.info("Generated CID : " + CID + " | request : " + request.getRequestURI());
+    log.info("Generated CID : " + CID + " | Request : " + request.getRequestURI());
     MDC.put("CorrelationId", CID);
     return true;
   }
 
   @Override
   public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    log.info("Request end : " + request.getRequestURI() + " | Removing CID : " + MDC.get("CorrelationId"));
     MDC.remove("CorrelationId");
   }
 
